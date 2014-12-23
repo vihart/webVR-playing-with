76161e6568abf824b586d52f86800426df38d1d2
Henry Segerman <h@Henrys-MBP-4.att.net>
2014-12-23 11:04:37 -0800

    made VRControls_hyperbolic
    so that we can reinstate the original VRControls and not break other VR
    pages

---
+++ js/VRControls_hyperbolic.js
@@ -0,0 +1,163 @@
+/**
+ * @author dmarcos / https://github.com/dmarcos
+ with additions by https://github.com/hawksley and https://github.com/henryseg
+ */
+
+THREE.VRControls = function ( camera, done ) {
+
+	this._camera = camera;
+
+	this._init = function () {
+		var self = this;
+		if ( !navigator.mozGetVRDevices && !navigator.getVRDevices ) {
+			if ( done ) {
+				done("Your browser is not VR Ready");
+			}
+			return;
+		}
+		if ( navigator.getVRDevices ) {
+			navigator.getVRDevices().then( gotVRDevices );
+		} else {
+			navigator.mozGetVRDevices( gotVRDevices );
+		}
+		function gotVRDevices( devices ) {
+			var vrInput;
+			var error;
+			for ( var i = 0; i < devices.length; ++i ) {
+				if ( devices[i] instanceof PositionSensorVRDevice ) {
+					vrInput = devices[i]
+					self._vrInput = vrInput;
+					break; // We keep the first we encounter
+				}
+			}
+			if ( done ) {
+				if ( !vrInput ) {
+				 error = 'HMD not available';
+				}
+				done( error );
+			}
+		}
+	};
+
+	this._init();
+
+	this.manualRotation = quat.create();
+
+	this.manualControls = {
+      65 : {index: 1, sign: 1, active: 0},  // a
+      68 : {index: 1, sign: -1, active: 0}, // d
+      87 : {index: 0, sign: 1, active: 0},  // w
+      83 : {index: 0, sign: -1, active: 0}, // s
+      81 : {index: 2, sign: -1, active: 0}, // q
+      69 : {index: 2, sign: 1, active: 0},  // e
+      38 : {index: 3, sign: 1, active: 0},  // up
+      40 : {index: 3, sign: -1, active: 0}, // down
+      37 : {index: 4, sign: -1, active: 0}, // left
+      39 : {index: 4, sign: 1, active: 0},   // right
+      222 : {index: 5, sign: 1, active: 0}, // single quote
+      191 : {index: 5, sign: -1, active: 0},   // fwd slash
+      73 : {index: 7, sign: -1, active: 0},   // i
+      75 : {index: 7, sign: 1, active: 0},   // k
+      74 : {index: 6, sign: 1, active: 0},   // j
+      76 : {index: 6, sign: -1, active: 0}   // l
+
+    };
+
+	this.manualRotateRate = new Float32Array([0.0, 0.0, 0.0]);
+	this.manualMoveRate = new Float32Array([0.0, 0.0, 0.0]);
+	this.manualParabolicRate = new Float32Array([0.0, 0.0]);
+	this.updateTime = 0;
+
+	this.update = function() {
+
+		var camera = this._camera;
+		var vrState = this.getVRState();
+		var manualRotation = this.manualRotation;
+		var oldTime = this.updateTime;
+		var newTime = performance.now();
+		this.updateTime = newTime;
+
+		var interval = (newTime - oldTime) * 0.001;
+
+		///do translation 
+		var m, offset;
+		if (this.manualMoveRate[0] != 0 || this.manualMoveRate[1] != 0 || this.manualMoveRate[2] != 0){
+		    offset = getFwdVector().multiplyScalar(0.2 * interval * this.manualMoveRate[0]).add(
+		      		   getRightVector().multiplyScalar(0.2 * interval * this.manualMoveRate[1])).add(
+		      		   getUpVector().multiplyScalar(0.2 * interval * this.manualMoveRate[2]));
+		    m = translateByVector(offset);
+		    m.multiply(currentBoost);
+		    currentBoost.copy(m);
+		    }
+
+		//do parabolic motion
+		var m2, parabolicVector;
+		if (this.manualParabolicRate[0] != 0 || this.manualParabolicRate[1] != 0){
+			parabolicVector = new THREE.Vector2(0.2 * interval * this.manualParabolicRate[0], 
+												0.2 * interval * this.manualParabolicRate[1]);
+		    m2 = parabolicBy2DVector(parabolicVector);
+		    m2.multiply(currentBoost);
+		    currentBoost.copy(m2);
+		    }
+
+
+
+	  var update = quat.fromValues(this.manualRotateRate[0] * 0.2 * interval,
+	                               this.manualRotateRate[1] * 0.2 * interval,
+	                               this.manualRotateRate[2] * 0.2 * interval, 1.0);
+	  quat.normalize(update, update);
+	  quat.multiply(manualRotation, manualRotation, update);
+
+		if ( camera ) {
+			if ( !vrState ) {
+				camera.quaternion.fromArray(manualRotation);
+				return;
+			}
+
+			// Applies head rotation from sensors data.
+			var totalRotation = quat.create();
+      var state = vrState.hmd.rotation;
+      if (vrState.hmd.rotation[0] !== 0 ||
+					vrState.hmd.rotation[1] !== 0 ||
+					vrState.hmd.rotation[2] !== 0 ||
+					vrState.hmd.rotation[3] !== 0) {
+        quat.multiply(totalRotation, manualRotation, vrState.hmd.rotation);
+      } else {
+        totalRotation = manualRotation;
+      }
+
+			camera.quaternion.fromArray( totalRotation );
+		}
+
+	};
+
+	this.zeroSensor = function() {
+		var vrInput = this._vrInput;
+		if ( !vrInput ) {
+			return null;
+		}
+		vrInput.zeroSensor();
+	};
+
+	this.getVRState = function() {
+		var vrInput = this._vrInput;
+		var orientation;
+		var vrState;
+		if ( !vrInput ) {
+			return null;
+		}
+		orientation	= vrInput.getState().orientation;
+		vrState = {
+			hmd : {
+				rotation : [
+					orientation.x,
+					orientation.y,
+					orientation.z,
+					orientation.w
+				]
+			}
+		};
+		return vrState;
+	};
+
+};

3ddf6dc48bbf10c6088dceddc887cb6ebc757e7a
Henry Segerman <h@Henrys-MBP-4.att.net>
2015-01-04 16:00:34 -0800

    combining monkeys into one html file, nomDodec

--- js/centers_24_cell.js
+++ js/centers_24_cell.js
@@ -1,14 +1,3 @@
-
-function quatMult( p , q ) 
-{
-    var r = new THREE.Vector4();    
-    r.w = + p.w*q.w - p.x*q.x - p.y*q.y - p.z*q.z;
-    r.x = + p.w*q.x + p.x*q.w + p.y*q.z - p.z*q.y;
-    r.y = + p.w*q.y - p.x*q.z + p.y*q.w + p.z*q.x;
-    r.z = + p.w*q.z + p.x*q.y - p.y*q.x + p.z*q.w;    
-    return r;
-}
-
 var centers_24_cell = [];
 
 for (var i=0;i<2;i++){
---
+++ js/sphMath.js
@@ -0,0 +1,52 @@
+//spherical matrix functions
+
+THREE.Matrix4.prototype.add = function (m) {
+  this.set.apply(this, [].map.call(this.elements, function (c, i) { return c + m.elements[i] }));
+};
+
+function getFwdVector() {
+  return new THREE.Vector3(0,0,1).applyQuaternion(camera.quaternion);
+}
+function getRightVector() {
+  return new THREE.Vector3(-1,0,0).applyQuaternion(camera.quaternion);
+}
+function getUpVector() {
+  return new THREE.Vector3(0,-1,0).applyQuaternion(camera.quaternion);
+}
+
+function quatMult( p , q ) 
+{
+    var r = new THREE.Vector4();    
+    r.w = + p.w*q.w - p.x*q.x - p.y*q.y - p.z*q.z;
+    r.x = + p.w*q.x + p.x*q.w + p.y*q.z - p.z*q.y;
+    r.y = + p.w*q.y - p.x*q.z + p.y*q.w + p.z*q.x;
+    r.z = + p.w*q.z + p.x*q.y - p.y*q.x + p.z*q.w;    
+    return r;
+}
+
+function makeHopfColorMatrix( colourDir )
+{
+    //rotate colourDir to lie along (0,0,z,w), fixing (0,0,0,1) 
+    //http://math.stackexchange.com/questions/293116/rotating-one-3-vector-to-another
+    
+    var A = new THREE.Vector3(colourDir.x, colourDir.y, colourDir.z);
+    var B = new THREE.Vector3(0.,0.,1.0);
+    var X = new THREE.Vector3();
+    X.crossVectors(A,B);
+    X.normalize();
+    var theta = Math.acos( A.dot(B)/(A.length()*B.length())); ///dont care about sign
+    var m = new THREE.Matrix4().set(  0.,-X.z, X.y, 0., //input is row vectors
+                                     X.z,  0.,-X.x, 0.,
+                                    -X.y, X.x,  0., 0.,
+                                      0.,  0.,  0., 0.
+                        );
+    // console.log(m.elements);
+    var m2 = new THREE.Matrix4().copy(m).multiply(m);
+    m.multiplyScalar(Math.sin(theta));
+    m2.multiplyScalar(1.-Math.cos(theta));
+    var Rot = new THREE.Matrix4();
+    Rot.add(m);
+    Rot.add(m2);
+
+    return Rot;
+}
